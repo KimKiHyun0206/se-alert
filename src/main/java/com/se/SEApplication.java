cae7c3c190839ff03ac2def78ae25f506dbc9498
Duskafka <rlarlgus0206@naver.com>
2024-12-20 22:05:41 +0900

    initialCommit

---
+++ src/main/java/com/se/SEApplication.java
@@ -0,0 +1,11 @@
+package com.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SEApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SEApplication.class, args);
+    }
+}
