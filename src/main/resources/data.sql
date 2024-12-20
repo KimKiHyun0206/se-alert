98ee35f2c6c89f3b525a53e0db00125de535acc8
Duskafka <rlarlgus0206@naver.com>
2024-12-20 22:08:39 +0900

    feat: 초기 데이터 생성을 위한 data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+insert into permission (permission_name, notice, enroll)
+values ('STUDENT', 0, 0);
+
+insert into permission (permission_name, notice, enroll)
+values ('STUDENT_LEAVE', 0, 0);
+
+insert into permission (permission_name, notice, enroll)
+values ('GRADUATE', 0, 0);
+
+insert into permission (permission_name, notice, enroll)
+values ('LAB_LEADER', 1, 1);
+
+insert into permission (permission_name, notice, enroll)
+values ('ADMIN', 1, 1);
+
+
+insert into student (id, password, name, phone_number, about_me)
+values (202158031, 123456789, 'kim', '010-8380-8775', 'backend');
