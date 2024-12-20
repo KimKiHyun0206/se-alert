8ff6786a602572953ab14606fb63cb833b14b231
Duskafka <rlarlgus0206@naver.com>
2024-12-20 22:09:07 +0900

    feat(Util): 애플리케이션 전반에 걸쳐 사용될 util

---
+++ src/main/java/com/se/util/DateUtil.java
@@ -0,0 +1,18 @@
+package com.se.util;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class DateUtil {
+    public static Date getDate(){
+        return new Date();
+    }
+
+    public static LocalDateTime getLocalDateTime(){
+        return LocalDateTime.now();
+    }
+
+    public static Date getTokenValidTime(Date date, Long validTime){
+        return new Date(date.getTime() + validTime);
+    }
+}
